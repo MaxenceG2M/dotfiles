41fbc0bed25f50e75cf13cf9a3d6881cee60e7f9
Maxence Germain de Montauzan <mgermaindemontauzan@voyages-sncf.com>
2016-02-04 20:00:59 +0100

    Fix Pager first line problem + Tmux hour + doLink

---
+++ doLink.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/bash_logout ~/.bash_logout
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/emacs ~/.emacs
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/global-gitignore ~/.gitignore
+ln -s ~/dotfiles/inputrc ~/.inputrc
+ln -s ~/dotfiles/ls_colors ~/.ls_colors
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/zsh_aliases ~/.zsh_aliases
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
