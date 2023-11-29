1f15b1c5f606e29a5ac5e13627aea70fa5ad82b5
Maxence G. de Montauzan <maxence@gdemontauzan.fr>
2023-11-30 00:11:05 +0100

    (utils) Improve get scope scripts
    Permit to limit to X commits

--- get_scopes.sh
+++ get_scopes.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-git log --format="%C(auto) %h %s" --all --branches | grep -oP '\(\K[^\)]+'| sort | uniq -c | sort -n
+if [[ $1 ]]; then
+    NB_COMMIT=-$1
+    echo "Take last $1 commits"
+fi;
+
+git log --format="%C(auto) %h %s" --all --branches ${NB_COMMIT} | cut -d ' ' -f 3 | grep -oP '\(\K[^\)]+'| sort | uniq -c | sort -n
